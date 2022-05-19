4e5ff933361383fd442fdf871c5482e3d337287c
江鎮宇 <s1081809@gm.pu.edu.tw>
2022-05-19 20:57:31 +0800

    新增檔案

--- web410817982/Controllers/UserController.cs
+++ web410817982/Controllers/UserController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using web410817982.ViewModels;
 
 namespace web410817982.Controllers
 {
@@ -11,28 +13,17 @@ namespace web410817982.Controllers
         // GET: User
         public ActionResult SignUp()
         {
-            return View();
+            return View(new SignUpData());
         }
         [HttpPost]
-        public ActionResult SignUp(string name,string account,string password)
+        public ActionResult SignUp(SignUpData data)
         {
-            if(name == null || name =="")
+            
+            if (ModelState.IsValid)
             {
-                ViewBag.NameMessage = "請輸入姓名";
+                data.Message = "註冊成功";
             }
-            if (account == null || account == "")
-            {
-                ViewBag.AccountMessage = "請輸入帳號";
-            }
-            if (password == null || password == "")
-            {
-                ViewBag.PasswordMessage = "請輸入密碼";
-            }
-            if (name!="" && account!="" && password!="" )
-            {
-                ViewBag.Message = "註冊成功";
-            }
-            return View();
+            return View(data);
         }
     }
 }
